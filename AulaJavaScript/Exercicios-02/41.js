e75f9beb829a3bd442d03bcca0be1e736678e97f
Contagiovaneines <contatogiovaneines@gmail.com>
2024-07-31 15:52:23 -0300

    new

--- AulaJavaScript/Exercicios-02/41.js
+++ AulaJavaScript/Exercicios-02/41.js
@@ -15,10 +15,27 @@ Média de Aproveitamento   | Conceito
 */
 
 // Dados das notas e média dos exercícios
-const nota1 = 7.0;          // Nota 1
-const nota2 = 8.5;          // Nota 2
-const nota3 = 6.0;          // Nota 3
-const mediaExercicios = 7.5; // Média dos exercícios
+/*
+Faça um algoritmo para ler as 3 notas obtidas por um aluno nas 3 verificações e a média dos exercícios que fazem parte da avaliação.
+Calcular a média de aproveitamento usando a fórmula abaixo e escrever o conceito do aluno de acordo com a tabela de conceitos mais abaixo:
+
+Média_de_Aproveitamento = (N1 + N2 * 2 + N3 * 3 + Média_dos_Exercícios) / 7
+
+A atribuição de conceitos obedece à tabela abaixo:
+
+Média de Aproveitamento   | Conceito
+--------------------------|---------
+>=  9,0                   | A
+>=  7,5 e <  9,0          | B
+>=  6,0  e <  7,5         | C
+<  6,0                    | D
+*/
+
+// Dados das notas e média dos exercícios
+let nota1 = 7.0;          // Nota 1
+let nota2 = 8.5;          // Nota 2
+let nota3 = 6.0;          // Nota 3
+let mediaExercicios = 7.5; // Média dos exercícios
 
 // Calcula a média de aproveitamento
 const mediaAproveitamento = (nota1 + (nota2 * 2) + (nota3 * 3) + mediaExercicios) / 7;
